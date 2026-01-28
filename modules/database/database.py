8806aadee4b9cdf32191285992999dd2a97c30f7
A1-X1 <daa237@outlook.com>
2026-01-28 20:22:59 +0000

    - Added exit button

--- modules/database/database.py
+++ modules/database/database.py
@@ -2,5 +2,3 @@ import sqlite3
 
 connection = sqlite3.connect("modules/database/main.db")
 cursor = connection.cursor()
-
-cursor.execute('DELETE FROM GlobalSettingsTable WHERE (id=1);')
--- ui/pages/dashboard.py
+++ ui/pages/dashboard.py
@@ -1,4 +1,4 @@
-from nicegui import ui
+from nicegui import ui, app
 from modules.globalSettings import globalSettings
 from ui.pages.components.sideButton import SideButton
 
@@ -8,6 +8,20 @@ from ui.pages.components.sideButton import SideButton
 class DashboardLayout:
     def __init__(self):
         self.__settings = globalSettings
+
+    def exit_sequence(self):
+        with ui.dialog() as dialog, ui.card().classes('p-6'):
+            ui.label('Are you sure you want to exit?').classes('text-lg font-bold')
+            ui.label('Unsaved simulation data may be lost.').classes('text-sm text-gray-500')
+            
+            with ui.row().classes('w-full justify-end mt-4'):
+                # Close only the dialog
+                ui.button('Cancel', on_click=dialog.close).props('flat color=positive')
+                
+                # Close the entire application
+                ui.button('Exit', on_click=app.shutdown).props('unelevated color=negative')
+        
+        dialog.open()
         
 
     def render(self, active_route: str):
@@ -30,6 +44,11 @@ class DashboardLayout:
 
                 body {{ background-color: {colours.background} !important; }}
                 .my-sidebar {{ background-color: {colours.surface} !important; }}
+
+                .no-hover .q-focus-helper {{
+                    display: none !important;
+                }}
+                
             </style>
         ''')
 
@@ -37,9 +56,19 @@ class DashboardLayout:
         sidebar_style = f'background-color: {colours.surface}; padding: 0; padding-bottom: 20px;'
         
         with ui.left_drawer().style(sidebar_style).classes('flex flex-col items-center gap-0'):
-            
-            # App Title
-            ui.label('FINVEST').style(f'color: {colours.text_primary}; font-size: 24px; font-weight: bold; margin-bottom: 20px;').classes('p-5 flex items-center justify-center w-full ignore-bold')
+
+            with ui.row().classes('w-full relative-position items-center justify-center p-5'):
+                
+                # The Exit Button
+                ui.button(icon='logout', on_click=self.exit_sequence) \
+                    .props('flat round dense :ripple="false"') \
+                    .style(f'color: {colours.text_primary} !important;') \
+                    .classes('absolute-left q-ml-md h-full no-hover') 
+
+                # The Title
+                ui.label('FINVEST') \
+                    .style(f'color: {colours.text_primary}; font-size: 24px; font-weight: bold;') \
+                    .classes('ignore-bold')
 
             # Navigation Logic
             SideButton(
