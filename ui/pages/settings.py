ad1a4c67a501779af3d60b346ec5391f0b182dd0
A1-X1 <daa237@outlook.com>
2026-01-30 14:42:12 +0000

    - Relevant comments to settings page

--- ui/pages/settings.py
+++ ui/pages/settings.py
@@ -13,12 +13,13 @@ class SettingsPage:
         self.__settings = globalSettings
         self.__currencyInput : SettingsInput = None
 
-    def placeholder():
-        pass
-
+    # stores the new settings in the database creates table if it doesn't exist
     def storeNewSettings(self, newSettings : GlobalSettings):
 
+        # serialise the settings object
         binaryData = pickle.dumps(newSettings)
+
+        # create table if it doesn't exist
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS GlobalSettingsTable (
                 id INTEGER PRIMARY KEY CHECK (id = 1), 
@@ -26,16 +27,20 @@ class SettingsPage:
             )
         """)
 
+        # insert or replace the settings object
         cursor.execute("""
             INSERT OR REPLACE INTO GlobalSettingsTable (id, data) 
             VALUES (1, ?)
         """, (binaryData,))
 
+        # commit the changes
         connection.commit()
 
+    # toggles dark mode and stores the new settings
     def toggleDarkMode(self):
         settings = globalSettings
 
+        # toggle the dark mode (boolean inversion logic)
         if (settings.darkMode == True):
             settings.theme = Theme
             settings.darkMode = False
@@ -45,12 +50,14 @@ class SettingsPage:
 
         self.storeNewSettings(settings)
 
-        # UI CALLBACK: Force Refresh
+        # ui callback to force refresh
         ui.run_javascript('window.location.reload()')
 
+    # toggles accessibility mode and stores the new settings
     def toggleAccessibilityMode(self):
         settings = globalSettings
 
+        # toggle the accessibility mode (boolean inversion logic)
         if (settings.accessibilityMode == True):
             settings.fontWeight = 500
             settings.accessibilityMode = False
@@ -60,30 +67,36 @@ class SettingsPage:
 
         self.storeNewSettings(settings)
 
-        # UI CALLBACK: Force Refresh
+        # ui callback to force refresh
         ui.run_javascript('window.location.reload()')
 
+    # updates the currency and stores the new settings
     def updateCurrency(self, currency : str):
         settings = globalSettings
         
+        # get the currency object
         newCurrency = get_currency_by_code(currency)
 
+        # if the currency doesn't exist, exit the function
         if (newCurrency == None):
             return
         else:
             settings.currency = newCurrency
             settings.currencySymbol = CurrencySymbols.get_symbol(currency)
 
+        # store the new settings
         self.storeNewSettings(settings)
 
-        # UI CALLBACK: Force Refresh
+        # ui callback to force refresh
         ui.run_javascript('window.location.reload()')
 
+    # renders the settings page
     def render(self):
         colours = self.__settings.theme
         
         ui.label('Settings').style(f'color: {colours.text_primary}; font-size: 200%')
 
+        # render the settings tiles with relevant callbacks
         SettingsTile("Dark mode", "dark_mode", "#5d47ff", "white", self.__settings.darkMode, lambda: self.toggleDarkMode())
         SettingsTile("Accessibility mode", "visibility", "#cf9800", "white", self.__settings.accessibilityMode, lambda: self.toggleAccessibilityMode())
         self.__currencyInput = SettingsInput("Currency", "monetization_on", "#08a112", "white", self.__settings.currency.code, lambda newCurrency : self.updateCurrency(newCurrency))
