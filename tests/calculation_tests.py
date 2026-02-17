e0277d52fcdffb247c941be1729630bb0e3898c8
A1-X1 <daa237@outlook.com>
2026-02-17 18:25:58 +0000

    - more tests

--- tests/calculation_tests.py
+++ tests/calculation_tests.py
@@ -1,9 +1,19 @@
+import sys
+import os
+
+# Adds the parent directory to the system path to prevent import errors
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from modules.calculations import calculateSharpeRatio, calculateVolatility, calculateTotalReturn, generate_gbm_paths
-import pandas as pd
 
-# testing gbm
+# testing gbm with params
+s0 = 100
+drift = 0.1
+volatility = -0.5
+numSims = 10
+numSteps = 100
+dt = 1/252
 
-path1 = generate_gbm_paths(100, 0.1, 0.2, 10, 100, 1/252)
-
+path1 = generate_gbm_paths(s0, drift, volatility, numSims, numSteps, dt)
 
 print(path1)
